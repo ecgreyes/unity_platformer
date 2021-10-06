d7dcbbbe4b168ead736be6aac7d7ec0bdafa301a
ecgreyes <emanuel.reyes@obf.ateneo.edu>
2021-10-06 18:01:06 +0800

    Changed collision and movement

--- PlatformerGame/Assets/Scripts/Movement.cs
+++ PlatformerGame/Assets/Scripts/Movement.cs
@@ -5,22 +5,24 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
 
+    private Rigidbody2D rb2d;
+
     public float runSpeed = 2f;
+    public float horizontalMovement;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalMovement = Input.GetAxis("Horizontal");
     }
 
     private void FixedUpdate() {
-        var movement = Input.GetAxis("Horizontal");
-        transform.position += new Vector3(movement, 0, 0) * Time.fixedDeltaTime * runSpeed;
+        rb2d.velocity = new Vector2(horizontalMovement * runSpeed, rb2d.velocity.y);
     }
 }
